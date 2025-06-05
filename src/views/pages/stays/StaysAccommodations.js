b37b2b169dafcbdfb3915d1cdaeba6bee43f2c4f
sama <sghavamzadeh46@gmail.com>
2025-06-05 16:18:33 +0330

    complete accomodation section

--- src/views/pages/stays/StaysAccommodations.js
+++ src/views/pages/stays/StaysAccommodations.js
@@ -1,6 +1,23 @@
+import SimpleBreadcrumb from "views/components/BreadCrumbs";
+import AccommodationTable from "./AccomodationTable";
+import { Button } from "reactstrap";
 const StaysAccommodations = () => {
+  const breadcrumbItems = [
+    { label: "Home", to: "/admin/dashboard" },
+    { label: "Stays management", to: "/admin/stays" },
+    { label: "Stays Info graphics" },
+  ];
   return (
-    <div>StaysAccommodations</div>
-  )
-}
-export default StaysAccommodations
+    <div className="content">
+      <div className="d-flex justify-content-between align-items-center">
+        <SimpleBreadcrumb
+          items={breadcrumbItems}
+          olClassName="screenshot-style-breadcrumb"
+        />
+        <Button color="info">Add new accommodation</Button>
+      </div>
+      <AccommodationTable />
+    </div>
+  );
+};
+export default StaysAccommodations;
