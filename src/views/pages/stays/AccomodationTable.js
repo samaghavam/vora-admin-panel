9f560b7753d8f0dc70b4a4c8c035a5ffa01c9fa3
sama <sghavamzadeh46@gmail.com>
2025-07-01 19:39:42 +0330

    update accomodation table

--- src/views/pages/stays/AccomodationTable.js
+++ src/views/pages/stays/AccomodationTable.js
@@ -1,5 +1,4 @@
 import React from "react";
-import classNames from "classnames";
 import {
   Card,
   CardBody,
@@ -9,9 +8,14 @@ import {
   Col,
   Button,
 } from "reactstrap";
-import ReactTable from "components/ReactTable/ReactTable.js";
+import SweetAlert from "react-bootstrap-sweetalert";
 
-const accommodationDataTable = [
+import ReactTable from "components/ReactTable/ReactTable.js";
+import EditModal from "components/editModal/EditModal";
+import ViewModal from "components/viewModal/ViewModal";
+
+// Example flight data
+const dataTable = [
   ["Airi Satou", "Grand Hyatt Tokyo", "2025-11-01", "2025-11-05", 2, "ACC001"],
   ["Angelica Ramos", "The Ritz London", "2025-12-10", "2025-12-15", 1, "ACC002"],
   ["Ashton Cox", "Fairmont San Francisco", "2026-01-20", "2026-01-25", 3, "ACC003"],
@@ -22,158 +26,145 @@ const accommodationDataTable = [
   ["Cedric Kelly", "Burj Al Arab Jumeirah", "2026-03-01", "2026-03-07", 1, "ACC008"],
   ["Charde Marshall", "The Peninsula Hong Kong", "2025-12-20", "2025-12-27", 2, "ACC009"],
   ["Colleen Hurst", "Aman Tokyo", "2026-04-05", "2026-04-12", 1, "ACC010"],
+
 ];
 
-const AccommodationTable = () => {
-  const [data, setData] = React.useState(
-    accommodationDataTable.map((prop, key) => {
-      return {
-        id: key,
-        name: prop[0],
-        accommodationName: prop[1],
-        checkInDate: prop[2],
-        checkOutDate: prop[3],
-        reservedRooms: prop[4],
-        reservationNumber: prop[5],
-        actions: (
-          <div className="actions-right">
-            <Button
-              onClick={() => {
-                let obj = data.find((o) => o.id === key);
-                alert(
-                  "You've clicked VIEW button on \n{ \nName: " +
-                  obj.name +
-                  ", \nAccommodation: " +
-                  obj.accommodationName +
-                  ", \nCheck-in: " +
-                  obj.checkInDate +
-                  ", \nCheck-out: " +
-                  obj.checkOutDate +
-                  ", \nRooms: " +
-                  obj.reservedRooms +
-                  ", \nReservation No: " +
-                  obj.reservationNumber +
-                  "\n}."
-                );
-              }}
-              color="info"
-              size="sm"
-              className={classNames("btn-icon btn-link like", {
-                "btn-neutral": key < 5, 
-              })}
-              title="View"
-            >
-              <i className="tim-icons icon-zoom-split" />
-            </Button>{" "}
-            {/* Edit button */}
-            <Button
-              onClick={() => {
-                let obj = data.find((o) => o.id === key);
-                alert(
-                  "You've clicked EDIT button on \n{ \nName: " +
-                  obj.name +
-                  ", \nAccommodation: " +
-                  obj.accommodationName +
-                  ", \nCheck-in: " +
-                  obj.checkInDate +
-                  ", \nCheck-out: " +
-                  obj.checkOutDate +
-                  ", \nRooms: " +
-                  obj.reservedRooms +
-                  ", \nReservation No: " +
-                  obj.reservationNumber +
-                  "\n}."
-                );
-              }}
-              color="warning"
-              size="sm"
-              className={classNames("btn-icon btn-link edit", {
-                "btn-neutral": key < 5,
-              })}
-              title="Edit"
-            >
-              <i className="tim-icons icon-pencil" />
-            </Button>{" "}
-            {/* Remove button */}
-            <Button
-              onClick={() => {
-                if (window.confirm("Are you sure you want to delete this reservation?")) {
-                    setData(currentData => currentData.filter(item => item.id !== key));
-                }
-              }}
-              color="danger"
-              size="sm"
-              className={classNames("btn-icon btn-link remove", {
-                "btn-neutral": key < 5,
-              })}
-              title="Remove"
-            >
-              <i className="tim-icons icon-simple-remove" />
-            </Button>
-          </div>
-        ),
-      };
-    })
-  );
+const ReservationTable = () => {
+  const [data, setData] = React.useState([]);
+  const [alert, setAlert] = React.useState(null);
+  
+  // State for Edit Modal
+  const [editModalOpen, setEditModalOpen] = React.useState(false);
+  const [originalItem, setOriginalItem] = React.useState(null);
+  const [draftItem, setDraftItem] = React.useState(null);
+
+  // State for View Modal
+  const [viewModalOpen, setViewModalOpen] = React.useState(false);
+  const [viewingItem, setViewingItem] = React.useState(null);
+
+  // Modal field configuration (used by both modals)
+  const ticketFields = [
+    { key: 'name', label: 'Name' },
+    { key: 'from', label: 'From' },
+    { key: 'destination', label: 'Destination' },
+    { key: 'airline', label: 'Airline' },
+    { key: 'date', label: 'Flight Date' },
+    { key: 'fair', label: 'Fair' },
+    { key: 'airplane', label: 'Airplane' },
+    { key: 'flightDuration', label: 'Flight duration' },
+    { key: 'reservationNumber', label: 'Flight Number' },
+    { key: 'state', label: 'State' }
+  ];
+  
+  // Format initial data
+  React.useEffect(() => {
+    const formattedData = dataTable.map((prop, key) => ({
+      id: key,
+      name: prop[0], from: prop[1], destination: prop[2], airline: prop[3], date: prop[4], reservationNumber: prop[5],
+      fair: key % 3 === 0 ? 'Business' : 'Economy',
+      airplane: key % 2 === 0 ? 'Boeing 777' : 'Airbus A350',
+      flightDuration: `${Math.floor(Math.random() * 5) + 10}h ${Math.floor(Math.random() * 60)}m`,
+      state: 'Upcoming',
+    }));
+    setData(formattedData);
+  }, []);
+
+  const handleEditClick = (row) => {
+    setOriginalItem(row);
+    setDraftItem({ ...row });
+    setEditModalOpen(true);
+  };
+
+  const handleModalInputChange = (fieldKey, value) => {
+    setDraftItem(prev => ({ ...prev, [fieldKey]: value }));
+  };
+
+  const handleSave = () => {
+    setData(prev => prev.map(item => item.id === originalItem.id ? draftItem : item));
+    setEditModalOpen(false);
+  };
+
+  const handleRemoveClick = (id) => {
+    setAlert(
+      <SweetAlert
+        warning style={{ display: "block", marginTop: "-100px" }} title="Are you sure?"
+        onConfirm={() => { setData(currentData => currentData.filter(item => item.id !== id)); setAlert(null); }}
+        onCancel={() => setAlert(null)}
+        confirmBtnBsStyle="danger" cancelBtnBsStyle="secondary" confirmBtnText="Yes, delete it!" cancelBtnText="Cancel" showCancel
+      >
+        You will not be able to recover this reservation!
+      </SweetAlert>
+    );
+  };
+  
+  // **THE FIX IS HERE:** This function now correctly opens the View Modal.
+  // The previous version incorrectly used alert(), causing the error.
+  const handleViewClick = (row) => {
+    setViewingItem(row);
+    setViewModalOpen(true);
+  };
 
   return (
     <>
+      {alert}
       <div className="content">
-        <Row className="mt-5"> 
+        <Row className="mt-5">
           <Col xs={12} md={12}>
             <Card>
-              <CardHeader>
-                <CardTitle tag="h4">Accommodation Reservations</CardTitle> 
-              </CardHeader>
+              <CardHeader><CardTitle tag="h4">React Table</CardTitle></CardHeader>
               <CardBody>
                 <ReactTable
-                  data={data}
-                  filterable
-                  resizable={false}
+                  data={data.map(item => ({
+                    ...item,
+                    actions: (
+                      <div className="actions-right">
+                        <Button onClick={() => handleViewClick(item)} color="info" size="sm" className="btn-icon btn-link like" title="View"><i className="tim-icons icon-zoom-split" /></Button>{" "}
+                        <Button onClick={() => handleEditClick(item)} color="warning" size="sm" className="btn-icon btn-link edit" title="Edit"><i className="tim-icons icon-pencil" /></Button>{" "}
+                        <Button onClick={() => handleRemoveClick(item.id)} color="danger" size="sm" className="btn-icon btn-link remove" title="Remove"><i className="tim-icons icon-simple-remove" /></Button>
+                      </div>
+                    )
+                  }))}
+                  filterable resizable={false}
                   columns={[
-                    {
-                      Header: "NAME",
-                      accessor: "name",
-                    },
-                    {
-                      Header: "ACCOMMODATION NAME", 
-                      accessor: "accommodationName",
-                    },
-                    {
-                      Header: "CHECK IN DATE", 
-                      accessor: "checkInDate",
-                    },
-                    {
-                      Header: "CHECK OUT DATE", 
-                      accessor: "checkOutDate",
-                    },
-                    {
-                      Header: "RESERVED ROOMS", 
-                      accessor: "reservedRooms",
-                    },
-                    {
-                      Header: "RESERVATION NUMBER", 
-                      accessor: "reservationNumber",
-                    },
-                    {
-                      Header: "ACTIONS", 
-                      accessor: "actions",
-                      sortable: false,
-                      filterable: false,
-                    },
+                    { Header: "NAME", accessor: "name" }, { Header: "From", accessor: "from" },
+                    { Header: "Destination", accessor: "destination" }, { Header: "Airline", accessor: "airline" },
+                    { Header: "Date", accessor: "date" }, { Header: "Reservation number", accessor: "reservationNumber" },
+                    { Header: "Actions", accessor: "actions", sortable: false, filterable: false },
                   ]}
-                  defaultPageSize={10}
-                  showPaginationTop
-                  showPaginationBottom={false}
-                  className="-striped -highlight" 
+                  defaultPageSize={10} showPaginationTop showPaginationBottom={false} className="-striped -highlight"
                 />
               </CardBody>
             </Card>
           </Col>
         </Row>
       </div>
+
+      <EditModal
+        isOpen={editModalOpen}
+        onClose={() => setEditModalOpen(false)}
+        title="Edit Ticket Details"
+        fields={ticketFields}
+        data={draftItem}
+        onSave={handleSave}
+        onInputChange={handleModalInputChange}
+      />
+      
+      {/* Render the new ViewModal */}
+      <ViewModal
+        isOpen={viewModalOpen}
+        onClose={() => setViewModalOpen(false)}
+        title="Ticket Details"
+        fields={ticketFields}
+        data={viewingItem}
+      />
+
+      <style>{`
+        .ReactTable .actions-right .btn-icon i {
+          color: white !important;
+        }
+      `}</style>
     </>
   );
 };
 
-export default AccommodationTable;
+export default ReservationTable;
